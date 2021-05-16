2dde1a5a63ab49bd04072f9fcd0ca8c6a0da9b6e
ts-silver <tenstarserver@gmail.com>
2021-05-16 20:02:13 +0800

    Add length check

--- randtoken/randset.go
+++ randtoken/randset.go
@@ -13,6 +13,9 @@ var LetterRunesHex = []rune("1234567890ABCDEFabcdef")
 
 //RandStringRunes generate string with fixed length of provided rune set
 func RandStringRunes(n int, runes []rune) string {
+	if n <= 0 {
+		return ""
+	}
 	sb := strings.Builder{}
 	for i := 0; i < n; i++ {
 		num, _ := rand.Int(rand.Reader, big.NewInt(int64(len(runes))))
