166d274eb38ba2fd12fcc23d865aa2f39f62f62a
Silver Xia <silverbelial@gmail.com>
2021-05-16 20:00:45 +0800

    returns error from rand.Int for RandStringRunes

--- randtoken/randset.go
+++ randtoken/randset.go
@@ -12,11 +12,14 @@ var letterRunesWX = []rune("1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 var LetterRunesHex = []rune("1234567890ABCDEFabcdef")
 
 //RandStringRunes generate string with fixed length of provided rune set
-func RandStringRunes(n int, runes []rune) string {
+func RandStringRunes(n int, runes []rune) (string, error) {
 	sb := strings.Builder{}
 	for i := 0; i < n; i++ {
-		num, _ := rand.Int(rand.Reader, big.NewInt(int64(len(runes))))
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(runes))))
+		if err != nil {
+			return "", err
+		}
 		sb.WriteRune(runes[num.Int64()])
 	}
-	return sb.String()
+	return sb.String(), nil
 }
